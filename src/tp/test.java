d87f71801a399e52ca8714bfa7f1b9c3c89bcda0
rija <randrianarivony@rijandrisolo.ovh>
2022-06-07 19:57:45 +0200

    cours java envoi approche-imperative

---
+++ src/tp/test.java
@@ -0,0 +1,18 @@
+package tp;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 0 ;
+		if (i == 0) {
+			System.out.println("i est nul");
+		}else if(i > 0){
+			System.out.println("i est positif");
+		}else {
+			System.out.println("i est négatif");
+		}
+
+	}
+}
